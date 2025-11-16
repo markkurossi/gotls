b9475e54fb55547dab8e51343382fedc155c587a
Markku Rossi <mtr@iki.fi>
2025-11-16 09:57:37 +0100

    Removed verbose test outputs.

--- crypto/tls/alert_test.go
+++ crypto/tls/alert_test.go
@@ -25,36 +25,23 @@ func TestAlertsAsErrors(t *testing.T) {
 	}
 
 	err = fmt.Errorf("Certificate validation failed: %w", AlertBadCertificate)
-	fmt.Printf("err: %v\n", err)
 	if !errors.As(err, &tlsAlert) {
 		t.Errorf("%v is not tls.AlertDescription\n", err)
-	} else {
-		fmt.Printf("wrapped error: %v\n", tlsAlert)
 	}
 
 	err = fmt.Errorf("write %w failed: %w", AlertDecodeError, io.EOF)
-	fmt.Printf("err: %v\n", err)
 	if !errors.As(err, &tlsAlert) {
 		t.Errorf("%v is not tls.AlertDescription\n", err)
-	} else {
-		fmt.Printf("wrapped error: %v\n", tlsAlert)
 	}
 
 	err = fmt.Errorf("write failed: %w: %w", io.EOF, AlertDecryptError)
-	fmt.Printf("err: %v\n", err)
 	if !errors.As(err, &tlsAlert) {
 		t.Errorf("%v is not tls.AlertDescription\n", err)
-	} else {
-		fmt.Printf("wrapped error: %v\n", tlsAlert)
 	}
 
 	inner := fmt.Errorf("write %w failed: %w", AlertDecryptError, io.EOF)
 	err = fmt.Errorf("failed to decode packet: %w", inner)
-	fmt.Printf("err: %v\n", err)
 	if !errors.As(err, &tlsAlert) {
 		t.Errorf("%v is not tls.AlertDescription\n", err)
-	} else {
-		fmt.Printf("wrapped error: %v\n", tlsAlert)
 	}
-
 }
